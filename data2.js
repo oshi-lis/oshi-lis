5dbafabc76ce4209e3dc79675fa060d7f2dbc726
oshi-lis <makkuro.kurosuke2135@gmail.com>
2026-04-13 18:08:46 +0900

    Create data2.js

---
+++ data2.js
@@ -0,0 +1,19 @@
+const data = {
+  vtubers: [
+    {
+      name: "テスト用",
+      streams: [
+        {
+          date: "2026-04-01",
+          title: "歌枠",
+          songs: [
+            {
+              name: "テスト曲",
+              url: "https://youtube.com"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
